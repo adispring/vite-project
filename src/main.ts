22638a7720b97c50bb550137c444511df5a924c2
adispring <sunnyadi@163.com>
2022-03-10 23:19:02 +0800

    vue2 can run

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+import Vue from 'vue';
+import App from './App.vue';
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+new Vue({
+  render: h => h(App)
+}).$mount('#app');
