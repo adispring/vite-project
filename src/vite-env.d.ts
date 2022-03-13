0963cbdf1b2c2954d7a33ecd0ae2398c0d6b7364
adispring <sunnyadi@163.com>
2022-03-13 08:30:31 +0800

    feat: 开始配置解析

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,4 +5,7 @@ declare module '*.vue' {
   // // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   // const component: DefineComponent<{}, {}, any>
   // export default component
+  import Vue = require('vue')
+  const value: Vue.ComponentOptions<Vue>
+  export default value
 }
