94c1cd4f3ae65dbd71a45fe0e2cd8efaf0620e0a
adispring <sunnyadi@163.com>
2022-03-15 23:48:01 +0800

    feat: vite 配置代理

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,14 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '/src/pages'),
+      }
+    }
   }
 })
